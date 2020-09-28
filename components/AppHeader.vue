7ab9bf0549ddeef94e89f0d17a9f4a5ba9038f03
drcyph3r <sb4514.work@gmail.com>
2020-09-28 23:03:52 +0530

    ft: bottom nav

---
+++ components/AppHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="w-full flex justify-between md:justify-end md:space-x-4">
+    <!-- logo on mobile -->
+    <img class="md:hidden" src="~/assets/logo.svg" width="24" alt="logo" />
+    <!-- UNC Balance -->
+    <div class="unc-bal">0.0000 UNC</div>
+    <!-- Connect Wallet Button -->
+    <connect-wallet />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.unc-bal {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 4px 8px;
+  border-radius: 4px;
+  background: #dcd8f9;
+  color: var(--primary-color);
+  font-family: var(--s-font);
+  font-size: 12pt;
+  font-weight: 600;
+  cursor: pointer;
+}
+
+@media (max-width: theme('screens.md')) {
+  .unc-bal {
+    display: none;
+  }
+}
+</style>
---
+++ components/BottomNav.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="bottom-nav">
+    <!-- Nav Links -->
+    <div v-for="link in navLinks" :key="link.id">
+      <nuxt-link v-slot="{ isExactActive }" :to="link.route">
+        <nav-link
+          :to="link.route"
+          :icon="link.icon"
+          :name="link.title"
+          :is-active="isExactActive"
+        />
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navLinks: [
+        {
+          id: 1,
+          title: 'Overview',
+          icon: 'OverviewIcon',
+          route: '/',
+        },
+        {
+          id: 2,
+          title: 'Mint',
+          icon: 'MintIcon',
+          route: '/mint',
+        },
+        {
+          id: 3,
+          title: 'Unlock',
+          icon: 'UnlockIcon',
+          route: '/unlock',
+        },
+        {
+          id: 4,
+          title: 'Earn',
+          icon: 'EarnIcon',
+          route: '/earn',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.bottom-nav {
+  widows: 100%;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: white;
+  padding: 0.8rem 1rem 0.5rem 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border: 0.5px solid #00000010;
+  transition: 0.5s;
+}
+
+@media (min-width: theme('screens.md')) {
+  .bottom-nav {
+    display: none;
+  }
+}
+</style>
